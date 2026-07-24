8a09ceb3496dd4d9f2a5ba73d0b98cb2400d88b1
Husni_Kusuma <husnikusuma00@gmail.com>
2026-07-24 13:50:28 +0700

    fix: update URLs to use 'www' for consistency across the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://envoyou.com',
+  site: 'https://www.envoyou.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
